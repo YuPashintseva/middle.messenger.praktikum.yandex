8b4d06031394109abc3f007ddfb53de44782fb4f
Yuliya Russinova <juliyapashintseva@gmail.com>
2021-11-12 14:52:00 +0600

    fix

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    plugins: [
+        require('postcss-mixins'),
+        require('autoprefixer'),
+        require('postcss-simple-vars')
+    ]
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import {sum} from './modules/sum';
+import '../style.css'
 
 const root = document.querySelector('#root');
-root.textContent = sum(6, -1).toString(); 
+root.textContent = sum(6, -1).toString();
+
+const Handlebars = require("handlebars");
+const template = Handlebars.compile("Name: {{name}}");
+const data = template({name: "Yuliya"});
+
+document.getElementsByTagName("h1")[0].textContent += data;
